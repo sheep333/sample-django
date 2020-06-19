5637463907ef92f1c9bee145e16b2b9709363ca4
sheep333 <yukiko.elike1.51@gmail.com>
2020-06-19 21:04:49 +0900

    タスクへのアクセス権限を追加

--- task/views.py
+++ task/views.py
@@ -1,18 +1,26 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, redirect
+
 from .models import MyTask
 
 
-class MyTaskListView(ListView):
+class MyTaskListView(LoginRequiredMixin, ListView):
     model = MyTask
     template_name = "task/task_list.html"
 
+    def get_queryset(self):
+        return MyTask.objects.filter(user=self.request.user)
 
+
+@login_required
 def update_mytask(request, task_id):
     if request.method == 'POST':
         mytask = get_object_or_404(MyTask, id=task_id)
-        mytask.finished = True
-        mytask.save()
+        if request.user == mytask.user:
+            mytask.finished = True
+            mytask.save()
     return redirect('task:list')
 
 
